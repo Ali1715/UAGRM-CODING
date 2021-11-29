1d4f1f8c7c80261f3b5b50750d00ced23a25c0c2
Ali1715 <95193346+Ali1715@users.noreply.github.com>
2021-11-28 22:41:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+.container1{
+	width:50%;
+	margin:auto;
+}
+.nav-main{
+font-size:17px;
+display:flex;
+justify-content:space-between;
+align-items:center;
+height:320x;
+padding:90px 0;
+}
+
+.nav-main ul li{
+	padding:20px;
+}
+.nav-main ul li a:hover {
+paddin :2px;
+}
+
+.nav-main ul.nav-menu{
+	flex:1;
+	margin-left:20px;
+}
+.bx bx-menu{
+	
+	
+	
+	
+	
+}
+
+
+<nav class="nav-main">
+	  <i class="bx bx-menu"></i>
+   <!--img scr="" alt="FLogo" class="nav-brand"-->
+ <ul class="nav-menu">
+    <li><a class="dropdown-item" href="#">FUNDAMENTOS DE LA PROGRAMACION</a></li>
+</ul>
+  </nav></div>
+    </div>
